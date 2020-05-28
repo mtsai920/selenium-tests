e90859cfe5a6a6afff7926ca1052160465822905
mtsai <megantsai920@gmail.com>
2020-05-28 11:02:35 -0400

    Added .gitignore file, added Youtube file

--- google.js
+++ google.js
@@ -1,19 +1,11 @@
 const { Builder, By, Key, until } = require("selenium-webdriver")
 
-// driver.get('http://google.com')
-
 const goGoogle = async function () {
   let driver = await new Builder().forBrowser('chrome').build()
     if (!driver) {
       console.log('Failed to get to site')
     } else {
       await driver.get('http://google.com')
-
-      // const input = driver.findElement(By.name('q'))
-      //
-      // input.click()
-      //
-      // input.sendKeys('medieval cats')
 
       const button = driver.findElement(By.className('Fx4vi'))
 
